d3f6fb54dc68819a46172b66926b6d0ae30aaefd
satoshi.marumoto <satoshi.marumoto@st-ventures.jp>
2020-04-16 18:59:04 +0900

    アプリアイコンにバッジを付与

--- AppIconBadgeSample/ViewController.swift
+++ AppIconBadgeSample/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // アプリ初期表示時にバッジ付与の許可を求める (初回はココでユーザに許可を得るダイアログが表示される)
+        UNUserNotificationCenter.current().requestAuthorization(options: .badge) { (granted, eror) in
+            if granted {
+                print("許可 OK")
+            }
+            else {
+                print("許可 NG")
+            }
+        }
     }
-
-
+    
+    // バッジの数を変更する
+    @IBAction func badgeNumberChangeButtonTapped(_ sender: Any) {
+        // 警告回避のためメインスレッドで実行
+        DispatchQueue.main.async {
+            // バッジの数をインクリメントする
+            UIApplication.shared.applicationIconBadgeNumber += 1
+        }
+    }
+    
+    // リセットボタンバッジの数を0 (非表示) に変更
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        DispatchQueue.main.async {
+            // 0 を代入するとバッジが消える
+            UIApplication.shared.applicationIconBadgeNumber = 0
+        }
+    }
 }
-
